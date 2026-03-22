304d6f2384a0bb30d7849cf6dc868ef3033cbab9
RR <rrn091301@gmail.com>
2026-03-22 17:37:18 +0800

    feat(server/user): add user service layer with error handling

---
+++ server/src/modules/user/user.service.ts
@@ -0,0 +1,69 @@
+import CustomNotFoundError from '../../utils/errors/NotFoundError.js';
+
+import * as userRepo from './user.repository.js';
+
+export async function createUser({ username, password_hash, avatar_color }) {
+  const user = await userRepo.createUser({
+    username,
+    password_hash,
+    avatar_color,
+  });
+
+  if (!user) {
+    throw new Error('User not created');
+  }
+
+  return user;
+}
+
+export async function getUsers() {
+  return userRepo.getUsers();
+}
+
+export async function getUser({ id, current_user_id }) {
+  const user = await userRepo.getUser({ id, current_user_id });
+
+  if (!user) {
+    throw new CustomNotFoundError('User not found');
+  }
+
+  return user;
+}
+
+export async function updateUserProfile({ id, display_name, pronouns, avatar_url, bio }) {
+  const isUserProfileUpdated = await userRepo.updateUserProfile({
+    id,
+    display_name,
+    pronouns,
+    avatar_url,
+    bio,
+  });
+
+  if (!isUserProfileUpdated) {
+    throw new Error('User profile not updated');
+  }
+}
+
+export async function updateUsername({ id, username }) {
+  const isUsernameUpdated = await userRepo.updateUsername({ id, username });
+
+  if (!isUsernameUpdated) {
+    throw new Error('Username not updated');
+  }
+}
+
+export async function updateUserAvatar({ id, avatar_url }) {
+  const isUserAvatarUpdated = await userRepo.updateUserAvatar({ id, avatar_url });
+
+  if (!isUserAvatarUpdated) {
+    throw new Error('User avatar not updated');
+  }
+}
+
+export async function deleteUser({ id }) {
+  const isUserDeleted = await userRepo.deleteUser({ id });
+
+  if (!isUserDeleted) {
+    throw new Error('User not deleted');
+  }
+}
