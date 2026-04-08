f52969188837acdb1bb98b16c6e8c51db9297bf0
RR <rrn091301@gmail.com>
2026-04-08 19:28:47 +0800

    fix: bind server to 0.0.0.0 for Render deployment

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,6 @@ import 'dotenv/config';
 import app from './app.js';
 import { PORT } from './config/env.js';
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
