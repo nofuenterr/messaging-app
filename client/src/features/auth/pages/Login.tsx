bb5601600df44052f684a89c7d6ea387867ff899
RR <rrn091301@gmail.com>
2026-04-20 16:32:15 +0800

    feat: display server error messages on login form

--- client/src/features/auth/pages/Login.tsx
+++ client/src/features/auth/pages/Login.tsx
@@ -1,20 +1,21 @@
 import { EyeClosedIcon, EyeOpenIcon } from '@radix-ui/react-icons';
+import { isAxiosError } from 'axios';
 import { unstable_PasswordToggleField as PasswordToggleField } from 'radix-ui';
-import type { SubmitEvent } from 'react';
+import { useState, type SubmitEvent } from 'react';
 import { Link } from 'react-router-dom';
 
 import ScrollArea from '../../../components/ui/ScrollArea';
 import { useLogin } from '../auth.queries';
 
 export default function LoginForm() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
   const login = useLogin();
+
+  const errorMessage = isAxiosError(login.error) ? login.error.response?.data?.message : null;
 
   const handleSubmit = (e: SubmitEvent<HTMLFormElement>) => {
     e.preventDefault();
-
-    const form = e.currentTarget;
-    const username = (form.elements.namedItem('username') as HTMLInputElement).value;
-    const password = (form.elements.namedItem('password') as HTMLInputElement).value;
 
     login.mutate({ username, password });
   };
@@ -28,18 +29,22 @@ export default function LoginForm() {
           <form onSubmit={handleSubmit} className="grid w-full gap-6">
             <fieldset className="grid w-full gap-4">
               <input
+                required
                 type="text"
                 name="username"
                 placeholder="Username"
                 maxLength={30}
                 className="bg-input-bg text-placeholder rounded-full px-4 py-2 focus:outline-none"
+                onChange={(e) => setUsername(e.target.value)}
               />
 
               <PasswordToggleField.Root>
                 <div className="relative">
                   <PasswordToggleField.Input
+                    required
                     name="password"
                     placeholder="Password"
+                    onChange={(e) => setPassword(e.target.value)}
                     className="bg-input-bg text-placeholder w-full rounded-full px-4 py-2 focus:outline-none"
                   />
 
@@ -53,10 +58,12 @@ export default function LoginForm() {
               </PasswordToggleField.Root>
             </fieldset>
 
+            {errorMessage && <p className="text-danger text-start text-sm">{errorMessage}</p>}
+
             <button
               className="bg-info disabled:bg-info-soft disabled:text-light-600 hover:bg-info-hover cursor-pointer rounded-lg px-4 py-2 font-medium"
               type="submit"
-              disabled={login.isPending}
+              disabled={login.isPending || !(username && password)}
             >
               {login.isPending ? 'Logging in...' : 'Login'}
             </button>
