c08eaf51bce2628e4f5b0fe68ac5ad1c1df4e0e5
RR <rrn091301@gmail.com>
2026-04-20 16:23:57 +0800

    feat: add types to passport and pool

--- server/src/modules/auth/auth.routes.ts
+++ server/src/modules/auth/auth.routes.ts
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+import type { PassportStatic } from 'passport';
+import type { Pool } from 'pg';
 
 import * as validationMiddleware from '../../middleware/validation.middleware.js';
 
@@ -6,7 +8,7 @@ import * as authController from './auth.controller.js';
 
 // "/auth"
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export default (passport, pool) => {
+export default (passport: PassportStatic, pool: Pool) => {
   const authRouter = Router();
 
   authRouter.post('/login', authController.authenticateLogin(passport));
